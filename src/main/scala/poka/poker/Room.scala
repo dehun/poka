a796c89185176feedea50bab457c40a010085d0a
ynetesov <yuriy.netesov@gmail.com>
2018-02-21 09:20:35 +0100

    implementing room and deck

--- src/main/scala/poka/poker/Room.scala
+++ src/main/scala/poka/poker/Room.scala
@@ -30,10 +30,14 @@ object Room {
     case object WrongTurnOrder extends RoomError
     case object WrongMoveOrder extends RoomError
     case object InternalGameLogicError extends RoomError
+    case object TooManyPlayers extends RoomError
+    case object InsufficientFunds extends RoomError
   }
 
   case class Stack(value:Int)
   case class PlayerState(putInAPot:Int, stackLeft:Stack, placeOnTable:Int, cards:Set[Card]) {
+    def call(amount: Int): Either[RoomError.InsufficientFunds.type , PlayerState] = Right(this.copy(putInAPot))
+
     val isAllIn:Boolean = stackLeft.value == 0
   }
 }
@@ -57,19 +61,32 @@ case class Room(players:Map[PlayerId, PlayerState], folded:Set[PlayerId], roomRo
     } yield nm._1
   }
 
-  private lazy val isAllBetsMatched = {
-    val biggestBet = players.map({case (pid, ps) => ps.putInAPot}).max
-    val allInPlayers = players.filter({case (pid, ps) => ps.isAllIn}).keySet -- folded
-    val inGamePlayers = players.keySet -- folded -- allInPlayers
-    inGamePlayers.exists {pid => players(pid).putInAPot != biggestBet}
-  }
+  private lazy val allInPlayers = players.filter({case (pid, ps) => ps.isAllIn}).keySet -- folded
+  private lazy val inGamePlayers = players.keySet -- folded -- allInPlayers
+  private lazy val biggestBet = players.map({case (pid, ps) => ps.putInAPot}).max
+  private lazy val isAllBetsMatched = inGamePlayers.exists {pid => players(pid).putInAPot != biggestBet}
 
   import Room._
   def moveToNextRound(mover:PlayerId):Room = roomRound match {
-    case RoomRound.PreFlop =>
-      val (newDeck, f::s::t::Nil) = deck.pollN(3)
-      ???
-
+    case RoomRound.PreFlop => {
+      for {
+        (newDeck, f :: s :: t :: Nil) <- deck.pollN(3)
+        (newnewDeck, cardsForPlayers) <- deck.pollN(inGamePlayers.size)
+        playersWithCards = inGamePlayers.zip(cardsForPlayers.grouped(2).toStream).foldLeft(this.players) {
+          case (acc, (pid, cards)) => acc.updated(pid, acc(pid).copy(cards = cards.toSet))
+        }
+      } yield this.copy(roomRound = RoomRound.Flop((f, s, t)), players=playersWithCards, deck = newnewDeck)
+    }.get // TODO: either
+    case flop:RoomRound.Flop =>
+      val (newDeck, card) = deck.poll.get
+      this.copy(roomRound=RoomRound.Turn(card, flop), deck=newDeck)
+    case turn:RoomRound.Turn =>
+      val (newDeck, card) = deck.poll.get
+      this.copy(roomRound=RoomRound.River(card, turn), deck=newDeck)
+    case river:RoomRound.River =>
+      // TODO: calculate winner
+      this.copy(roomRound=RoomRound.Ended(Map.empty, river))
+    case _:RoomRound.Ended => this
   }
 
   def playerMove(playerId:PlayerId, move:PlayerMove):Either[RoomError, Room] = {
@@ -77,23 +94,26 @@ case class Room(players:Map[PlayerId, PlayerState], folded:Set[PlayerId], roomRo
     require((nextMover.isDefined && players.contains(nextMover.get) && folded.contains(nextMover.get)) || roomRound == PreFlop) // TODO: replace with guard
     require(!folded.contains(playerId)) // TODO: to guard
 
-    val next = nextMover.getOrElse(players.minBy(_._2.placeOnTable)._1)
-    if (playerId != next) Left(WrongTurnOrder)
+    val mover = nextMover.getOrElse(players.minBy(_._2.placeOnTable)._1)
+    if (playerId != mover) Left(WrongTurnOrder)
     else {
       move match {
         // one can always fold
         case PlayerMove.Fold =>
-          findNextMoverOnTable(next) match {
-            case Some(newMover) => Right(this.copy(folded=this.folded + playerId, nextMover=Some(newMover)))
-            case None =>
+          findNextMoverOnTable(mover) match {
+            case newMover@Some(_) => Right(this.copy(folded=this.folded + playerId, nextMover=newMover))
+            case None if isAllBetsMatched=> Right(moveToNextRound(mover))
+            case None => Left(RoomError.WrongMoveOrder)
           }
-
+        case PlayerMove.Check =>
+          findNextMoverOnTable(mover) match {
+            case newMover@Some(_) => Right(this.copy(nextMover=newMover))
+            case None if isAllBetsMatched => Right(moveToNextRound(mover))
+            case None => Left(RoomError.WrongMoveOrder)
+          }
+        case PlayerMove.Call(amount) =>
+          this.players(mover).call(amount).map(newPs => this.copy(players = this.players.updated(mover, newPs)))
       }
-      // previous players bet should be called or you fold
-      // in case if no bets have been placed one can check
-
-      //val previousPlayers
-      ???
     }
   }
 }
