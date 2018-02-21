04b95f1b1749360419209194ad51f0088bf4ba74
ynetesov <yuriy.netesov@gmail.com>
2018-02-21 18:07:01 +0100

    room++

--- src/main/scala/poka/poker/Room.scala
+++ src/main/scala/poka/poker/Room.scala
@@ -18,9 +18,15 @@ object Room {
   sealed trait RoomRound
   object RoomRound {
     case object PreFlop extends RoomRound
-    case class Flop(cards:(Card, Card, Card)) extends RoomRound
-    case class Turn(card:Card, flop: Flop) extends RoomRound
-    case class River(card:Card, turn: Turn) extends RoomRound
+    case class Flop(cards:(Card, Card, Card)) extends RoomRound {
+      lazy val allCards:Set[Card] = Set(cards._1, cards._2, cards._3)
+    }
+    case class Turn(card:Card, flop: Flop) extends RoomRound {
+      lazy val allCards:Set[Card] = flop.allCards + card
+    }
+    case class River(card:Card, turn: Turn) extends RoomRound {
+      lazy val allCards:Set[Card] = turn.allCards + card
+    }
     case class Ended(result:Map[PlayerId, Int], river: River) extends RoomRound
   }
 
@@ -37,8 +43,16 @@ object Room {
   case class Stack(value:Int)
   case class PlayerState(putInAPot:Int, stackLeft:Stack, placeOnTable:Int, cards:Set[Card]) {
     def call(amount: Int): Either[RoomError.InsufficientFunds.type , PlayerState] = Right(this.copy(putInAPot))
-
     val isAllIn:Boolean = stackLeft.value == 0
+
+    def takePot(maxShare:Int):(PlayerState, Int) = {
+      require(maxShare >= 0)
+
+      (this.copy(putInAPot=math.max(this.putInAPot - maxShare, 0)),
+        math.min(maxShare, putInAPot))
+    }
+
+    def winPot(win:Int):PlayerState = ???
   }
 }
 
@@ -46,10 +60,12 @@ case class PlayerId(id:String)
 
 case class Room(players:Map[PlayerId, PlayerState], folded:Set[PlayerId], roomRound:RoomRound, deck:Deck, nextMover:Option[PlayerId]) {
   def joinPlayer(playerId:PlayerId, stack:Stack):Room = {
-    val newPlayer = (playerId -> PlayerState(0, stack, this.players.size, Set.empty))
     if (roomRound == PreFlop) {
-      this.copy(players = this.players + newPlayer)
+      val (newDeck, cards) = deck.pollN(2).get // TODO either
+      val newPlayer = playerId -> PlayerState(0, stack, this.players.size, cards.toSet)
+      this.copy(players = this.players + newPlayer, deck=newDeck)
     } else {
+      val newPlayer = playerId -> PlayerState(0, stack, this.players.size, Set.empty)
       this.copy(players=this.players + newPlayer, folded=this.folded+playerId)
     }
   }
@@ -71,11 +87,7 @@ case class Room(players:Map[PlayerId, PlayerState], folded:Set[PlayerId], roomRo
     case RoomRound.PreFlop => {
       for {
         (newDeck, f :: s :: t :: Nil) <- deck.pollN(3)
-        (newnewDeck, cardsForPlayers) <- deck.pollN(inGamePlayers.size)
-        playersWithCards = inGamePlayers.zip(cardsForPlayers.grouped(2).toStream).foldLeft(this.players) {
-          case (acc, (pid, cards)) => acc.updated(pid, acc(pid).copy(cards = cards.toSet))
-        }
-      } yield this.copy(roomRound = RoomRound.Flop((f, s, t)), players=playersWithCards, deck = newnewDeck)
+      } yield this.copy(roomRound = RoomRound.Flop((f, s, t)), deck = newDeck)
     }.get // TODO: either
     case flop:RoomRound.Flop =>
       val (newDeck, card) = deck.poll.get
@@ -84,7 +96,23 @@ case class Room(players:Map[PlayerId, PlayerState], folded:Set[PlayerId], roomRo
       val (newDeck, card) = deck.poll.get
       this.copy(roomRound=RoomRound.River(card, turn), deck=newDeck)
     case river:RoomRound.River =>
-      // TODO: calculate winner
+      val handsInOrder = (inGamePlayers ++ allInPlayers)
+        .map{pid => (pid, Hand(players(pid).cards, river.allCards))}.toVector
+        .sortBy {case (pid, hand) => hand}
+        .map {case (pid, hand) => pid}
+        .zipWithIndex.toMap
+      val newPlayerStates = handsInOrder.keySet.foldLeft(players) { (initialNewPlayers, pid) =>
+        val winnerShare = initialNewPlayers(pid).putInAPot
+        initialNewPlayers.foldLeft(initialNewPlayers) {case (newPlayers, (lpid, lps)) =>
+          if (folded.contains(lpid) || (handsInOrder(lpid) < handsInOrder(pid))) {
+            val (newLps, potTakken) = lps.takePot(winnerShare)
+            newPlayers
+              .updated(lpid, newLps)
+              .updated(pid, newPlayers(pid).winPot(potTakken))
+          } else newPlayers
+        }
+      }
+
       this.copy(roomRound=RoomRound.Ended(Map.empty, river))
     case _:RoomRound.Ended => this
   }
